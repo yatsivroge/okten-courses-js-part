f436fc7d6fe54c02a8ff37893b4b0a92b8266a94
rostyslav <rostyslav.yatsiv1@gmail.com>
2022-01-13 22:12:20 +0200

    hw1

---
+++ react-app-okten/src/components/users/Users.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from 'react';
+
+const Users = () => {
+    return (
+        <div>
+            <span>LOLDALdsaldl</span>
+        </div>
+    )
+};
+
+export default Users;
